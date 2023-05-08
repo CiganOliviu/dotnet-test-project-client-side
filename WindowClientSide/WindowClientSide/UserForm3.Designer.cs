6c194ff37b7ce7c3d0e67a532b635e25bf03f7b2
Cigan Oliviu David <ciganoliviudavid@gmail.com>
2023-05-08 21:19:28 +0300

    updates

---
+++ WindowClientSide/WindowClientSide/UserForm3.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace WindowClientSide
+{
+    partial class UserForm3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.roleUpdateBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.emailUpdateBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.userLastNameUpdateBox = new System.Windows.Forms.TextBox();
+            this.sprintIdInput = new System.Windows.Forms.Label();
+            this.userUpdateIdBox = new System.Windows.Forms.TextBox();
+            this.userAdd = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userFirstNameUpdateBox = new System.Windows.Forms.TextBox();
+            this.Sprints = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(278, 473);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 16);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "role";
+            // 
+            // roleUpdateBox
+            // 
+            this.roleUpdateBox.Location = new System.Drawing.Point(281, 502);
+            this.roleUpdateBox.Name = "roleUpdateBox";
+            this.roleUpdateBox.Size = new System.Drawing.Size(199, 22);
+            this.roleUpdateBox.TabIndex = 39;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(278, 392);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 16);
+            this.label3.TabIndex = 38;
+            this.label3.Text = "email";
+            // 
+            // emailUpdateBox
+            // 
+            this.emailUpdateBox.Location = new System.Drawing.Point(281, 421);
+            this.emailUpdateBox.Name = "emailUpdateBox";
+            this.emailUpdateBox.Size = new System.Drawing.Size(199, 22);
+            this.emailUpdateBox.TabIndex = 37;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 311);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 16);
+            this.label2.TabIndex = 36;
+            this.label2.Text = "last name";
+            // 
+            // userLastNameUpdateBox
+            // 
+            this.userLastNameUpdateBox.Location = new System.Drawing.Point(281, 340);
+            this.userLastNameUpdateBox.Name = "userLastNameUpdateBox";
+            this.userLastNameUpdateBox.Size = new System.Drawing.Size(199, 22);
+            this.userLastNameUpdateBox.TabIndex = 35;
+            // 
+            // sprintIdInput
+            // 
+            this.sprintIdInput.AutoSize = true;
+            this.sprintIdInput.Location = new System.Drawing.Point(278, 160);
+            this.sprintIdInput.Name = "sprintIdInput";
+            this.sprintIdInput.Size = new System.Drawing.Size(115, 20);
+            this.sprintIdInput.TabIndex = 34;
+            this.sprintIdInput.Text = "identify user id";
+            // 
+            // userUpdateIdBox
+            // 
+            this.userUpdateIdBox.Location = new System.Drawing.Point(281, 191);
+            this.userUpdateIdBox.Name = "userUpdateIdBox";
+            this.userUpdateIdBox.Size = new System.Drawing.Size(199, 22);
+            this.userUpdateIdBox.TabIndex = 33;
+            // 
+            // userAdd
+            // 
+            this.userAdd.Location = new System.Drawing.Point(312, 580);
+            this.userAdd.Name = "userAdd";
+            this.userAdd.Size = new System.Drawing.Size(114, 32);
+            this.userAdd.TabIndex = 32;
+            this.userAdd.Text = "Add";
+            this.userAdd.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 231);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 16);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "first name";
+            // 
+            // userFirstNameUpdateBox
+            // 
+            this.userFirstNameUpdateBox.Location = new System.Drawing.Point(281, 260);
+            this.userFirstNameUpdateBox.Name = "userFirstNameUpdateBox";
+            this.userFirstNameUpdateBox.Size = new System.Drawing.Size(199, 22);
+            this.userFirstNameUpdateBox.TabIndex = 30;
+            // 
+            // Sprints
+            // 
+            this.Sprints.AutoSize = true;
+            this.Sprints.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Sprints.Location = new System.Drawing.Point(307, 78);
+            this.Sprints.Name = "Sprints";
+            this.Sprints.Size = new System.Drawing.Size(185, 36);
+            this.Sprints.TabIndex = 29;
+            this.Sprints.Text = "Update User";
+            // 
+            // UserForm3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 743);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.roleUpdateBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.emailUpdateBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userLastNameUpdateBox);
+            this.Controls.Add(this.sprintIdInput);
+            this.Controls.Add(this.userUpdateIdBox);
+            this.Controls.Add(this.userAdd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.userFirstNameUpdateBox);
+            this.Controls.Add(this.Sprints);
+            this.Name = "UserForm3";
+            this.Text = "UserForm3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox roleUpdateBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox emailUpdateBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox userLastNameUpdateBox;
+        private System.Windows.Forms.Label sprintIdInput;
+        private System.Windows.Forms.TextBox userUpdateIdBox;
+        private System.Windows.Forms.Button userAdd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userFirstNameUpdateBox;
+        private System.Windows.Forms.Label Sprints;
+    }
+}
---
+++ WindowClientSide/WindowClientSide/UserForm3.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowClientSide
+{
+    public partial class UserForm3 : Form
+    {
+        public UserForm3()
+        {
+            InitializeComponent();
+        }
+    }
+}
